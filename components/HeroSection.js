11ece5b2fa7ecbbbc04c012d5ce67b65e8b1c5cd
paolocalzone <paolocalzone@protonmail.com>
2022-09-06 10:28:13 +0200

    Edit title content

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,10 +2,8 @@ export default function HeroSection() {
   return (
     <div className="pt-12 text-center">
       <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight">
-        <span className="block xl:inline">
-          Create your lens handle on testnet!
-        </span>{" "}
-        <span className="block text-emerald-600 xl:inline">on-chain</span>
+        <span className="block xl:inline">Create your lens handle!</span>{" "}
+        <span className="block text-emerald-600 xl:inline">on testnet</span>
       </h1>
       <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
         Create your lens profile on the testnet and begin to play around with
